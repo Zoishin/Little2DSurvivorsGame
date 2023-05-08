bc86eb74db2db917ff5d51f9c8624b011e50f460
Zoishin <zwc23123@163.com>
2023-05-08 18:48:25 +0800

    add enemy

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 75
+
+func _ready():
+	pass
+	
+	
+func _process(delta):
+	var direction = get_direction_to_player()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+	
+
+func get_direction_to_player()->Vector2:
+	var player_nodes = get_tree().get_first_node_in_group("player") as Node2D
+	if player_nodes != null:
+		return (player_nodes.global_position - global_position).normalized()
+	return Vector2.ZERO
--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 
+const  MAX_SPEED = 200
+
 
 func _ready():
 	pass
 	
 
 func _process(delta):
-	pass
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+func get_movement_vector()->Vector2:
+	var movement_vector = Vector2.ZERO
 	
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+	return Vector2(x_movement, y_movement)
